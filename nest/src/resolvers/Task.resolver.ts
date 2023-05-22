f7b1be286cf722033b56d01da1f3f2c62f586826
Katsu30 <sc30.katsu@gmail.com>
2023-05-22 23:44:24 +0900

    .feat add mutation feat

--- nest/src/resolvers/Task.resolver.ts
+++ nest/src/resolvers/Task.resolver.ts
@@ -1,13 +1,22 @@
 import { Task } from 'src/models/Task.model';
 import { TaskService } from '../services/Task.service';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class TaskResolver {
-  constructor(private readonly TaskService: TaskService) {}
+  constructor(private readonly taskService: TaskService) {}
 
   @Query(() => [Task], { nullable: 'items' })
   getTasks(): Task[] {
-    return this.TaskService.getTasks();
+    return this.taskService.getTasks();
+  }
+
+  @Mutation(() => Task)
+  createTask(
+    @Args('name') name: string,
+    @Args('dueDate') dueDate: string,
+    @Args('description', { nullable: true }) description: string,
+  ): Task {
+    return this.taskService.createTask(name, dueDate, description);
   }
 }
