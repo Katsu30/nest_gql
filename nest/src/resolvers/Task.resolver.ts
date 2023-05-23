3ea8d57f3d7bdf47224f30dff707e887f1fba6a7
Katsu30 <sc30.katsu@gmail.com>
2023-05-23 22:53:39 +0900

    .feat add dto

--- nest/src/resolvers/Task.resolver.ts
+++ nest/src/resolvers/Task.resolver.ts
@@ -1,6 +1,7 @@
 import { Task } from 'src/models/Task.model';
 import { TaskService } from '../services/Task.service';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateTaskInput } from 'src/dto/createTask.input';
 
 @Resolver()
 export class TaskResolver {
@@ -12,11 +13,7 @@ export class TaskResolver {
   }
 
   @Mutation(() => Task)
-  createTask(
-    @Args('name') name: string,
-    @Args('dueDate') dueDate: string,
-    @Args('description', { nullable: true }) description: string,
-  ): Task {
-    return this.taskService.createTask(name, dueDate, description);
+  createTask(@Args('createTaskInput') createTaskInput: CreateTaskInput): Task {
+    return this.taskService.createTask(createTaskInput);
   }
 }
