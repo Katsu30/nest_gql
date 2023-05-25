314918f83cb7cb2b3ea211c53947e7d2c4e3cf22
Katsu30 <sc30.katsu@gmail.com>
2023-05-25 23:51:04 +0900

    .feat add delete task

--- nest/src/resolvers/Task.resolver.ts
+++ nest/src/resolvers/Task.resolver.ts
@@ -1,6 +1,6 @@
 import { Task as TaskModel } from 'src/models/Task.model';
 import { TaskService } from '../services/Task.service';
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateTaskInput } from 'src/dto/createTask.input';
 import { Task } from '@prisma/client';
 import { UpdateTaskInput } from 'src/dto/updateTask.input';
@@ -26,5 +26,10 @@ export class TaskResolver {
     @Args('updateTaskInput') updateTaskInput: UpdateTaskInput,
   ): Promise<Task> {
     return await this.taskService.updateTask(updateTaskInput);
+  }
+
+  @Mutation(() => TaskModel)
+  async deleteTask(@Args('id', { type: () => Int }) id: number): Promise<Task> {
+    return await this.taskService.deleteTask(id);
   }
 }
--- nest/src/services/Task.service.ts
+++ nest/src/services/Task.service.ts
@@ -30,4 +30,12 @@ export class TaskService {
       where: { id },
     });
   }
+
+  async deleteTask(id: number): Promise<Task> {
+    return await this.prismaService.task.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
